1ea9d0aa7a3e7b6a7c8ad33954819da6df31ea0c
rodri <rodri@10.134.195.203>
2020-02-25 10:52:10 -0700

    Updated table formatting to support longer task names

--- src/Resources/sqlite3_db.py
+++ src/Resources/sqlite3_db.py
@@ -117,19 +117,38 @@ class SQLite3DB:
         out = beginning + out
         print(out)
 
-    def format_string_to_table(self, str_to_fmt):
-        str_to_fmt = str_to_fmt.split(',')
-        fmt_out = '|'
+    def format_string_to_table(self, str_to_fmt, fmt_out = "", list_to_fmt = []):
+        if list_to_fmt == []:
+            str_to_fmt = str_to_fmt.split(',')
+        else:
+            str_to_fmt = list_to_fmt
+        next_line = []
+        fmt_out += '|'
         num_spaces = self.__ID_SIZE - len(str_to_fmt[0])
         fmt_out += " " * (num_spaces // 2)
         fmt_out += str_to_fmt[0]
+        next_line.append("")
         fmt_out += " " * (num_spaces // 2 + num_spaces % 2)
         fmt_out += "|"
         str_to_fmt = str_to_fmt[1:]
         for member in str_to_fmt:
+            if len(member) > self.__FIELD_SIZE:
+                next_line.append(member[self.__FIELD_SIZE:])
+                member = member[:self.__FIELD_SIZE]
+            else:
+                next_line.append("")
             num_spaces = self.__FIELD_SIZE - len(member)
             fmt_str = " " * (num_spaces // 2)
             fmt_str += member
             fmt_str += " " * (num_spaces // 2 + num_spaces % 2)
             fmt_out += fmt_str + '|'
+        if (self.check_list_empty(next_line) == False):
+            fmt_out += "\n"
+            fmt_out = self.format_string_to_table("", fmt_out, next_line)
         return fmt_out
+
+    def check_list_empty(self, l):
+        for el in l:
+            if el != "":
+                return False
+        return True
